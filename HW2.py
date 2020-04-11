953dd6c243284ad9136ee986d58a11234b4df668
שקד רצון <shakratz@gmail.com>
2020-04-11 09:15:53 +0300

    HW2 initiation

--- HW2.py
+++ HW2.py
@@ -18,35 +18,32 @@ I1 = IMG['I1']
 I2 = IMG['I2']
 
 # Choose parameters
-WindowSize = None # Add your value here!
-MaxIter = None # Add your value here!
-NumLevels = None # Add your value here!
+WindowSize = None  # Add your value here!
+MaxIter = None  # Add your value here!
+NumLevels = None  # Add your value here!
 
 # Compute optical flow using LK algorithm
-(u,v) = LucasKanadeOpticalFlow(I1,I2,WindowSize,MaxIter,NumLevels)
+(u, v) = LucasKanadeOpticalFlow(I1, I2, WindowSize, MaxIter, NumLevels)
 
 # Warp I2
-I2_warp = WarpImage(I2,u,v)
+I2_warp = WarpImage(I2, u, v)
 
 # The RMS should decrease as the warped image (I2_warp) should be more similar to I1
-print('RMS of original frames: '+ str(np.sum(np.sum(np.abs((I1-I2)**2)))))
-print('RMS of processed frames: ' + str(np.sum(np.sum(np.abs((I1-I2_warp)**2)))))
-
+print('RMS of original frames: ' + str(np.sum(np.sum(np.abs((I1 - I2) ** 2)))))
+print('RMS of processed frames: ' + str(np.sum(np.sum(np.abs((I1 - I2_warp) ** 2)))))
 
 # Plot I1,I2,I2_warp
-
-
 
 
 ###########################################3PART 2: Video Stabilization################################################
 
 # Choose parameters
-WindowSize = None # Add your value here!
-MaxIter= None # Add your value here!
-NumLevels = None # Add your value here!
+WindowSize = None  # Add your value here!
+MaxIter = None  # Add your value here!
+NumLevels = None  # Add your value here!
 
-#Load video file
-InputVidName='input.avi'
+# Load video file
+InputVidName = 'input.avi'
 
 # Stabilize video - save the stabilized video inside the function
-StabilizedVid = LucasKanadeVideoStabilization(InputVid,WindowSize,MaxIter,NumLevels)
+StabilizedVid = LucasKanadeVideoStabilization(InputVid, WindowSize, MaxIter, NumLevels)
