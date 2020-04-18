12f4aeeb5a6740d0b5b8b8b18113e8affa09a041
שקד רצון <shakratz@gmail.com>
2020-04-18 15:19:47 +0300

    Updated 18.4.20 15:20

--- HW2.py
+++ HW2.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import scipy.io as sio
 from ex2_funcsions import *
-
+import cProfile
+import pstats
+import io
 # FILL IN YOUR ID
 # ID1 = 200940500
 # ID2 = 204251144
 
-
+""""
 #####################################PART 1: Lucas-Kanade Optical Flow################################################
+
+
 
 # Load images I1,I2
 IMG = sio.loadmat('HW2_PART1_IMAGES.mat')
@@ -31,8 +35,8 @@ I2_warp = WarpImage(I2, u, v)
 # The RMS should decrease as the warped image (I2_warp) should be more similar to I1
 print('RMS of original frames: ' + str(np.sqrt(np.average(np.power(I2 - I1, 2)))))
 print('RMS of processed frames: ' + str(np.sqrt(np.average(np.power(I2_warp - I1, 2)))))
-"""print('RMS of original frames: ' + str(np.sum(np.sum(np.abs((I1 - I2) ** 2)))))
-print('RMS of processed frames: ' + str(np.sum(np.sum(np.abs((I1 - I2_warp) ** 2)))))"""
+
+
 
 # Plot I1,I2,I2_warp
 plt.subplot(1, 3, 1)
@@ -45,17 +49,17 @@ plt.subplot(1, 3, 3)
 plt.imshow(I2_warp, cmap='gray')
 plt.title('I2_warp'), plt.xticks([]), plt.yticks([])
 plt.show()
-
+"""
 ###########################################3PART 2: Video Stabilization################################################
-""""
+
 # Choose parameters
-WindowSize = None  # Add your value here!
-MaxIter = None  # Add your value here!
-NumLevels = None  # Add your value here!
+WindowSize = 5  # Add your value here!
+MaxIter = 10  # Add your value here!
+NumLevels = 4  # Add your value here!
 
 # Load video file
 InputVidName = 'input.avi'
 
 # Stabilize video - save the stabilized video inside the function
 StabilizedVid = LucasKanadeVideoStabilization(InputVidName, WindowSize, MaxIter, NumLevels)
-"""
+
--- ex2_funcsions.py
+++ ex2_funcsions.py
@@ -2,56 +2,46 @@ import math
 import cv2
 import numpy as np
 from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
 
-
+"""
 def LucasKanadeStep(I1, I2, WindowSize):
     hw = round(WindowSize / 2)  # hw = half window size
     num_rows, num_cols = I2.shape[:2]
     du = np.zeros((num_rows, num_cols))
     dv = np.zeros((num_rows, num_cols))
-    # Shifting x by 1
-    translation_matrix = np.float32([[1, 0, 1], [0, 1, 0]])
-    x_shifted = cv2.warpAffine(I2, translation_matrix, (num_cols, num_rows))
 
-    # Shifting y by 1
-    translation_matrix = np.float32([[1, 0, 0], [0, 1, 1]])
-    y_shifted = cv2.warpAffine(I2, translation_matrix, (num_cols, num_rows))
-
-    # Derivatives
-    Ix = I2 - x_shifted
-    Iy = I2 - y_shifted
+    Ix = cv2.Sobel(I2, cv2.CV_64F, 1, 0, ksize=5)
+    Iy = cv2.Sobel(I2, cv2.CV_64F, 0, 1, ksize=5)
 
     for i in range(hw, num_rows - hw):
         for j in range(hw, num_cols - hw):
+            B = []
             # Splitting to WindowSize
-            I1seg = I1[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten()
-            I2seg = I2[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten()
-            Ixseg = Ix[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten()
-            Iyseg = Iy[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten()
+            I1seg = I1[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten('F')
+            I2seg = I2[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten('F')
+            Ixseg = Ix[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten('F')
+            Iyseg = Iy[i - hw:i + hw + 1, j - hw:j + hw + 1].flatten('F')
 
             # B = [Ix Iy] as CS
-            B = np.array([np.column_stack(Ixseg), np.column_stack(Iyseg)])
-            B = np.reshape(B, (WindowSize ** 2, 2))
+            B = np.array([Ixseg, Iyseg]).T
             # It is the difference between frames
             It = I2seg - I1seg
 
             # Delta p
-            BBT = np.matmul(cv2.transpose(B), B)
-            BIt = np.matmul(cv2.transpose(B), It)
+            BTB = np.matmul(cv2.transpose(B), B)
 
-            # In case det(A)=0 it is not invertible - if close to 0 their is no motion (background)
-            if np.linalg.det(BBT) < 0.1:
-                Dp = [0, 0]
-            else:
-                Dp = -np.matmul(np.linalg.inv(BBT), BIt)
+            # In case det(BTB)=0 it is not invertible - if close to 0 their is no motion (background)
+            if np.linalg.det(BTB) != 0:
+                BTIt = np.matmul(cv2.transpose(B), It)
+                Dp = np.matmul(np.linalg.inv(BTB), BTIt)  # Without minus since Sobel contain minus
 
             # Store the du and dv in the array
             du[i, j] = Dp[0]
             dv[i, j] = Dp[1]
 
     return du, dv
-
-
+"""
 """def WarpImage(I, u, v):
     num_rows, num_cols = I.shape[:2]
     I_warp = np.ones((num_rows, num_cols)) * (-1)  # holes will be marked as "-1"
@@ -82,15 +72,48 @@ def LucasKanadeStep(I1, I2, WindowSize):
 
     return I_warp"""
 
+
+def LucasKanadeStep(I1, I2, WindowSize):
+    height = I1.shape[0]
+    width = I2.shape[1]
+    u = np.zeros((height, width))
+    v = np.zeros((height, width))
+
+    # Creating Matrices for Calculation #
+    Ix = cv2.Sobel(I1, cv2.CV_64F, 1, 0, ksize=5)
+    Iy = cv2.Sobel(I1, cv2.CV_64F, 0, 1, ksize=5)
+    It = I2 - I1
+
+    for i in range(0, height - WindowSize, WindowSize):
+        for j in range(0, width - WindowSize, WindowSize):
+            B = np.zeros((WindowSize ** 2, 2))
+            It_window = np.zeros((WindowSize ** 2, 1))
+            # Creating the Vectors #
+            for n in range(WindowSize):
+                for m in range(WindowSize):
+                    B[n * WindowSize + m, 0] = Ix[i + n, j + m]
+                    B[n * WindowSize + m, 1] = Iy[i + n, j + m]
+                    It_window[n * WindowSize + m] = It[i + n, j + m]
+
+            # In case det(A)=0 it is not invertible - if close to 0 their is no motion (background)
+            if np.linalg.det(np.matmul(B.T, B)) != 0:
+                dp = np.matmul(np.matmul(np.linalg.inv(np.matmul(B.T, B)), B.T), It_window)
+
+            u[i:i + WindowSize, j:j + WindowSize] = dp[0] * np.ones((WindowSize, WindowSize))
+            v[i:i + WindowSize, j:j + WindowSize] = dp[1] * np.ones((WindowSize, WindowSize))
+
+    return u, v
+
+
 def WarpImage(I, u, v):
     num_rows, num_cols = I.shape[:2]
-    v_vec = np.tile(np.linspace(0, num_cols - 1, num_cols), (num_rows, 1))
-    u_vec = np.tile(np.linspace(0, num_rows - 1, num_rows).reshape(-1, 1), (1, num_cols))
+    u_vec = np.tile(np.linspace(0, num_cols - 1, num_cols), (num_rows, 1))
+    v_vec = np.tile(np.linspace(0, num_rows - 1, num_rows).reshape(-1, 1), (1, num_cols))
     umov = np.matrix.flatten(u_vec + u).reshape(-1, 1)
     vmov = np.matrix.flatten(v_vec + v).reshape(-1, 1)
 
     grid_x, grid_y = np.mgrid[0:(num_rows - 1):num_rows * 1j, 0:(num_cols - 1):num_cols * 1j]
-    points = np.concatenate((umov, vmov), axis=1)
+    points = np.concatenate((vmov, umov), axis=1)
     values = np.matrix.flatten(I)
     I_warp = griddata(points, values, (grid_x, grid_y), method='linear')
 
@@ -104,19 +127,13 @@ def LucasKanadeOpticalFlow(I1, I2, WindowSize, MaxIter, NumLevels):
     I2_pyr = []
     I1_pyr.append(I1)
     I2_pyr.append(I2)
-    odd_flag = np.zeros((NumLevels,2))
+    odd_flag = np.zeros((NumLevels, 2))
     # Creating image pyramids
-    for pyrLevel in range(1, NumLevels):
-        dst = cv2.pyrDown(I1_pyr[pyrLevel - 1])
+    for pyrLevel in range(0, NumLevels - 1):
+        dst = cv2.pyrDown(I1_pyr[pyrLevel])
         I1_pyr.append(dst)
-        dst = cv2.pyrDown(I2_pyr[pyrLevel - 1])
+        dst = cv2.pyrDown(I2_pyr[pyrLevel])
         I2_pyr.append(dst)
-
-        # Create odd flag array for later u/v dim fix
-        if math.ceil(dst.shape[1]) % 2 == 1:
-            odd_flag[pyrLevel,1] = 1
-        if math.ceil(dst.shape[0]) % 2 == 1:
-            odd_flag[pyrLevel,0] = 1
 
     # creating u and v
     num_rows, num_cols = dst.shape[:2]
@@ -129,12 +146,15 @@ def LucasKanadeOpticalFlow(I1, I2, WindowSize, MaxIter, NumLevels):
         I1down = I1_pyr[pyrLevel]
         I2down = I2_pyr[pyrLevel]
 
-        for inter in range(MaxIter):
+        for it in range(MaxIter):
             # Move the image I2 according to Delta p
-            I_warp = WarpImage(I2down, u, v)
+            I2_warp = WarpImage(I2down, u, v)
 
             # One step - Calculate Delta p
-            (du, dv) = LucasKanadeStep(I1down, I_warp, WindowSize)
+            (du, dv) = LucasKanadeStep(I1down, I2_warp, WindowSize)
+
+            if du.any() == 0 and dv.any() == 0:  # If there is no more change in u and v - break
+                break
 
             # Update u and v
             u = u + du
@@ -142,17 +162,8 @@ def LucasKanadeOpticalFlow(I1, I2, WindowSize, MaxIter, NumLevels):
 
         if pyrLevel != 0:  # not the First level
 
-            # Because pyrUp rounds up, need to change it for odd picture
-            if odd_flag[pyrLevel-1,0] == 1:
-                dim0 = math.ceil(I1down.shape[0] * 2) - 1
-            else:
-                dim0 = math.ceil(I1down.shape[0] * 2)
-            if odd_flag[pyrLevel-1, 1] == 1:
-                dim1 = math.ceil(I1down.shape[1] * 2) - 1
-            else:
-                dim1 = math.ceil(I1down.shape[1] * 2)
-            dims = (dim1, dim0)
-
+            dims = (I1_pyr[pyrLevel - 1].shape[1], I1_pyr[pyrLevel - 1].shape[0])
+            # dim1=I1_pyr[pyrLevel-1].shape[1]
             # going up 1 pyramid level, increasing u,v
             u = cv2.pyrUp(u, dstsize=dims)
             v = cv2.pyrUp(v, dstsize=dims)
@@ -165,34 +176,40 @@ def LucasKanadeOpticalFlow(I1, I2, WindowSize, MaxIter, NumLevels):
 
 def LucasKanadeVideoStabilization(InputVid, WindowSize, MaxIter, NumLevels):
     orig_video = cv2.VideoCapture(InputVid)
-
+    HalfWt = (WindowSize ** 2) // 2
+    HalfWb = (WindowSize ** 2) // 2 + (WindowSize ** 2) % 2
     # video output parameters
     fourcc = orig_video.get(6)
     fps = orig_video.get(5)
-    frameSize = ((int(orig_video.get(3))-WindowSize**2), (int(orig_video.get(4))-WindowSize**2))
+    frameSize = ((int(orig_video.get(4)) - WindowSize ** 2), (int(orig_video.get(3)) - WindowSize ** 2))
+    frameSize = (int(orig_video.get(4)), int(orig_video.get(3)))
     hasFrames, I1 = orig_video.read()
 
     # define video output
     output_video = cv2.VideoWriter('StabilizedVid_200940500_204251144.avi', int(fourcc), fps, frameSize, isColor=False)
 
     # write first frame to output video
+    #output_video.write(I1[HalfWb:frameSize[0] - HalfWt, HalfWb:frameSize[1] - HalfWt])
     output_video.write(I1)
 
     # stabilization using LK with first frame
     I_gray = cv2.cvtColor(I1, cv2.COLOR_BGR2GRAY)
-    numFrames = int(InputVid.get(7))
-    size = (int(orig_video.get(3)), int(orig_video.get(4)))
+    numFrames = int(orig_video.get(7))
+    size = (int(orig_video.get(4)), int(orig_video.get(3)))
 
-    for i in range (1,numFrames):
-        v = np.zeros(size)
-        u = np.zeros(size)
+    v = np.zeros(size)
+    u = np.zeros(size)
+
+    for i in range(1, 30):
+        print('Frame number ' + str(i + 1) + ' from ' + str(numFrames) + ' is in process')
+
         hasFrames, frame = orig_video.read()
         if hasFrames:  # hasFrames returns a bool, if frame is read correctly - it will be True
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            (du, dv) = LucasKanadeOpticalFlow(I_gray, gray_frame, WindowSize, MaxIter,NumLevels)
-            u += du
-            v += dv
+            (du, dv) = LucasKanadeOpticalFlow(I_gray, gray_frame, WindowSize, MaxIter, NumLevels)
+            u += np.mean(du) * np.ones(size)
+            v += np.mean(dv) * np.ones(size)
             # Move the frame according to Delta p
             frame_warp = WarpImage(gray_frame, u, v)
 
@@ -201,12 +218,9 @@ def LucasKanadeVideoStabilization(InputVid, WindowSize, MaxIter, NumLevels):
 
             # write frame to output video
             frame_warp = cv2.cvtColor(frame_warp.astype(np.uint8), cv2.COLOR_GRAY2RGB)
-            output_video.write(frame_warp[0:frameSize[0], 0:frameSize[1]])
-
+            #output_video.write(frame_warp[HalfWb:frameSize[0] - HalfWt, HalfWb:frameSize[1] - HalfWt])
+            output_video.write(frame_warp)
         else:
             break
     orig_video.release()
     output_video.release()
-
-
-
